25a26bfccacdb3f69e493cdaea963a5b4181cf15
Nikola <nikola.lazarov@smart-soft.net>
2023-02-12 15:04:38 +0200

    Added the currency and currencyInfo classes and their relationships

---
+++ Data/Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models;
+
+public class Currency
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Code { get; set; }
+    
+    public ICollection<CurrencyInfo> CurrencyInfos { get; set; }
+}
---
+++ Data/Models/CurrencyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Models;
+
+public class CurrencyInfo
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    [ForeignKey("Currency")]
+    public Guid CurrencyId { get; set; }
+    public Currency Currency { get; set; }
+    
+    public double BuyRate { get; set; }
+    
+    public double SellRate { get; set; }
+}
