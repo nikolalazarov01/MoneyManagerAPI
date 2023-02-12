888660f2fb098cfff474bd1cea1aa9afb5b3a514
Nikola <nikola.lazarov@smart-soft.net>
2023-02-12 15:08:03 +0200

    Added base currency to the user model

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace Data.Models;
 
 public class User : IdentityUser
 {
-    
+    [ForeignKey("BaseCurrency")]
+    public Guid BaseCurrencyId { get; set; }
+    public Currency BaseCurrency { get; set; }
 }
