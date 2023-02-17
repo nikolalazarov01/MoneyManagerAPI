63e2fec47d166e4d7c91f641d5a223d3c546a6ca
Nikola <nikola.lazarov@smart-soft.net>
2023-02-17 21:42:31 +0200

    Added RegisterRequestDto's

---
+++ Data/DtoModels/RegisterRequestDtoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Data.DtoModels;
+
+public class RegisterRequestDtoModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ MoneyManager_API/DTO/RegisterRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Data.Models.DTO;
+
+public class RegisterRequestDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
