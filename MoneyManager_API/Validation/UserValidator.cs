69a59fd0e8b34ee5aa280ea98e731cc63a141c3e
Nikola <nikola.lazarov@smart-soft.net>
2023-02-19 14:03:41 +0200

    Added validator for the registration request dto

---
+++ MoneyManager_API/Validation/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models.DTO;
+using FluentValidation;
+
+namespace MoneyManager_API.Validation;
+
+public class UserValidator : AbstractValidator<RegisterRequestDto>
+{
+    public UserValidator()
+    {
+        this.RuleFor(u => u.UserName).NotEmpty();
+        this.RuleFor(u => u.Password).NotEmpty();
+        this.RuleFor(u => u.Email).NotEmpty();
+        this.RuleFor(u => u.Currency).NotEmpty();
+    }
+}
