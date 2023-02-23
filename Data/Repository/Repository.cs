a751f00fdc837687dad69bed4c55527757856f05
Nikola <nikola.lazarov@smart-soft.net>
2023-02-23 12:29:38 +0200

    Removed the user validation and created the 'CreateAsync' method in the Repository.cs

--- Data/Repository/Repository.cs
+++ Data/Repository/Repository.cs
@@ -1,8 +1,60 @@
-﻿using Data.Repository.Contracts;
+﻿using System.Linq.Expressions;
+using Data.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Utilities;
 
 namespace Data.Repository;
 
 public class Repository<T> : IRepository<T> where T : class
 {
-    
+    private readonly DbContext _db;
+
+    public Repository(DbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<OperationResult> CreateAsync(T entity, CancellationToken token)
+    {
+        var operationResult = new OperationResult();
+
+        if (operationResult.ValidateNotNull(entity) == false) return operationResult;
+
+        try
+        {
+            await this._db.AddAsync(entity, token);
+            await this._db.SaveChangesAsync(token);
+        }
+        catch (Exception ex)
+        {
+            operationResult.AppendException(ex);
+        }
+
+        return operationResult;
+    }
+
+    public Task<OperationResult> DeleteAsync(T entity, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OperationResult> UpdateAsync(T entity, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OperationResult<T>> GetAsync(IEnumerable<Expression<Func<T, bool>>> func, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OperationResult<IEnumerable<T>>> GetManyAsync(IEnumerable<Expression<Func<T, bool>>> func, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OperationResult<bool>> AnyAsync(IEnumerable<Expression<Func<T, bool>>> func, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Utilities/OperationResultExtensions.cs
+++ Utilities/OperationResultExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Utilities;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Utilities;
 
 public static class OperationResultExtensions
 {
@@ -27,4 +30,18 @@ public static class OperationResultExtensions
         var error = new Error { IsNotExpected = true, Message = ex.Message };
         principal.AddError(error);
     }
+    
+#nullable enable
+    public static bool ValidateNotNull<TValue>(this OperationResult operationResult, [NotNullWhen(true)] TValue? value, [CallerFilePath] string? filePath = null, [CallerMemberName] string? memberName = null, [CallerLineNumber] int line = -1, [CallerArgumentExpression("value")] string? expression = null)
+    {
+        if (operationResult is null) throw new ArgumentNullException(nameof(operationResult));
+        if (value is not null) return true;
+
+        var error = new Error { Message = $"{FormatErrorMessage(filePath, memberName, line, expression)} should not be null." };
+        operationResult.AddError(error);
+        return false;
+    }
+#nullable restore
+    
+    private static string FormatErrorMessage(string filePath, string memberName, int line, string argumentExpression) => $"{filePath} ({memberName};{line}) - Expression [{argumentExpression}]";
 }
