3fd9e4a14a93087a89d64b9c90463accdec783a6
Nikola <nikola.lazarov@smart-soft.net>
2023-02-23 12:21:16 +0200

    Deleted the login and register dto's

--- Data/DTO/LoginRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Data.Models.DTO.Base;
-
-namespace Data.Models.DTO;
-
-public class LoginRequestDto
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
--- Data/DTO/LoginResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Data.Models.DTO.Base;
-
-namespace Data.Models.DTO;
-
-public class LoginResponseDto : BaseDtoModel
-{
-    public string? UserName { get; set; }
-    public string Token { get; set; }
-}
