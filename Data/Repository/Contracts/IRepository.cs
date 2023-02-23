892877bf5795a7549d65fee8a916e3d42851e3e2
Nikola <nikola.lazarov@smart-soft.net>
2023-02-23 12:22:27 +0200

    Added the methods for the repository in the interface

--- Data/Repository/Contracts/IRepository.cs
+++ Data/Repository/Contracts/IRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Data.Repository.Contracts;
+﻿using System.Linq.Expressions;
+using Utilities;
+
+namespace Data.Repository.Contracts;
 
 public interface IRepository<T> where T : class
 {
-    
+    Task<OperationResult> CreateAsync(T entity, CancellationToken token);
+    Task<OperationResult> DeleteAsync(T entity, CancellationToken token);
+    Task<OperationResult> UpdateAsync(T entity, CancellationToken token);
+    Task<OperationResult<T>> GetAsync(IEnumerable<Expression<Func<T, bool>>> func, CancellationToken token);
+    Task<OperationResult<IEnumerable<T>>> GetManyAsync(IEnumerable<Expression<Func<T, bool>>> func,
+        CancellationToken token);
+    Task<OperationResult<bool>> AnyAsync(IEnumerable<Expression<Func<T, bool>>> func, CancellationToken token);
 }
