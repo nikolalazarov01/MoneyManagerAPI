d77700397d0235333e8d1512e087cf25fe43eda0
Nikola <nikola.lazarov@smart-soft.net>
2023-02-25 12:22:16 +0200

    Added the account controller

---
+++ MoneyManager_API/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyManager_API.Controllers;
+
+[ApiController]
+[Route("/accounts")]
+public class AccountController : ControllerBase
+{
+    private readonly IUnitOfWork _services;
+    private IMapper _mapper;
+
+    public AccountController(IUnitOfWork services, IMapper mapper)
+    {
+        _services = services;
+        _mapper = mapper;
+    }
+}
