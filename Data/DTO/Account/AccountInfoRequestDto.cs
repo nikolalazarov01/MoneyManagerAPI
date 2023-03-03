2ae807cd8d754444e9dc717d8497d5964a30eb91
Nikola <nikola.lazarov@smart-soft.net>
2023-03-03 15:52:34 +0200

    Created AccountInfoRequestDto.cs

---
+++ Data/DTO/Account/AccountInfoRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Models.DTO.Account;
+
+public class AccountInfoRequestDto
+{
+    public Guid AccountId { get; set; }
+
+    public string Type { get; set; }
+    
+    public double Total { get; set; }
+}
