5cebcc1bf0cc8d3b5333a9fa14dce51badbbeb6a
Nikola <nikola.lazarov@smart-soft.net>
2023-03-03 15:16:05 +0200

    Created the TransactionController.cs

---
+++ MoneyManager_API/Controllers/TransactionController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyManager_API.Controllers;
+
+[ApiController]
+public class TransactionController : ControllerBase
+{
+    private readonly IUnitOfWork _services;
+    private readonly IMapper _mapper;
+
+    public TransactionController(IUnitOfWork services, IMapper mapper)
+    {
+        _services = services;
+        _mapper = mapper;
+    }
+}
