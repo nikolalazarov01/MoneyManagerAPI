e5d8246f0785bcd658299ffde54e78458be27d39
Nikola <nikola.lazarov@smart-soft.net>
2023-03-03 15:56:54 +0200

    Created the MakeTransaction method

--- MoneyManager_API/Controllers/TransactionController.cs
+++ MoneyManager_API/Controllers/TransactionController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Core.Contracts;
+using Data.Models.DTO.Account;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MoneyManager_API.Extensions;
 
 namespace MoneyManager_API.Controllers;
 
@@ -14,5 +17,20 @@ public class TransactionController : ControllerBase
     {
         _services = services;
         _mapper = mapper;
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> MakeTransaction([FromBody] AccountInfoRequestDto accountInfoDto, CancellationToken token)
+    {
+        var validationResult = await this._services.Accounts.ValidateTransaction(accountInfoDto, token);
+        if (!validationResult.IsSuccessful) return this.Error(validationResult);
+
+        var result = await this._services.Accounts.MakeTransaction(accountInfoDto, token);
+        if (!result.IsSuccessful) return this.Error(result);
+
+        var representation = this._mapper.Map<AccountDto>(result.Data);
+
+        return Ok(representation);
     }
 }
