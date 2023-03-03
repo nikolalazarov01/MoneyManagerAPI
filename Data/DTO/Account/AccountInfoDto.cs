1f28071f6a7c0eb4c4cc12039e731fa1e4c77714
Nikola <nikola.lazarov@smart-soft.net>
2023-03-03 15:01:52 +0200

    Added AccountInfoDto.cs and added it as collection in AccountDto.cs

---
+++ Data/DTO/Account/AccountInfoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Models.DTO.Account;
+
+public class AccountInfoDto
+{
+    public Guid Id { get; set; }
+
+    public string Type { get; set; }
+    
+    public double Total { get; set; }
+    
+    public DateTime Date { get; set; }
+}
