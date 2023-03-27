30e575035e47846d9c8ad0f169fc4000d129d5b4
Nikola <nikola.lazarov@smart-soft.net>
2023-03-27 19:24:39 +0300

    Added the QueryOptions.cs class and IQueryOptions.cs interface

---
+++ Core/Contracts/Options/IQueryOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using Data.Models.Contracts;
+
+namespace Core.Contracts.Options;
+
+public interface IQueryOptions<T> where T : class, IEntity
+{
+    IReadOnlyCollection<Expression<Func<T, bool>>> Filters { get; }
+    IReadOnlyCollection<Func<IQueryable<T>, IQueryable<T>>> Transformations { get; }
+}
---
+++ Core/Options/QueryOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using Core.Contracts.Options;
+using Data.Models.Contracts;
+
+namespace Core.Options;
+
+public class QueryOptions<T> : IQueryOptions<T> where T : class, IEntity
+{
+    private readonly List<Expression<Func<T, bool>>> _filters = new();
+    private readonly List<Func<IQueryable<T>, IQueryable<T>>> _transformations = new();
+
+    public IReadOnlyCollection<Expression<Func<T, bool>>> Filters => this._filters.AsReadOnly();
+
+    public IReadOnlyCollection<Func<IQueryable<T>, IQueryable<T>>> Transformations =>
+        this._transformations.AsReadOnly();
+
+    public bool AddFilter(Expression<Func<T, bool>>? filter)
+    {
+        if (filter is null) return false;
+        
+        this._filters.Add(filter);
+        return true;
+    }
+
+    public bool AddTransformation(Func<IQueryable<T>, IQueryable<T>>? transformation)
+    {
+        if (transformation is null) return false;
+        
+        this._transformations.Add(transformation);
+        return true;
+    }
+}
